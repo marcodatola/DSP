4047a2015b4b6f9005c291fde1751b7b67737cd8
Marco Datola <marcodatolaa@gmail.com>
2016-11-03 20:50:58 +0000

    added codes and lab report

---
+++ ex24.m
@@ -0,0 +1,26 @@
+[data, count] = readbuoydata('045200603.txt');
+
+x = data.date;
+y = data.Tp;
+M = [5 21 51];
+figure;
+
+% M-order 5
+for mm = 1:length(M)
+    
+    subplot(3,1,mm);
+    plot(x,y,'Color',[0.8 0.8 0.8],'Linewidth',2);
+    hold on;
+    avg = ex23(y,M(mm));
+    plot(x,avg,'k');
+    axis([x(1) x(end) 0 22]);
+    set(gca,'XTick',[x(1):(x(end) - x(1))/15:x(end)]);
+    grid on;
+    ylabel('Peak Period (s)');
+    datetick('x',6,'keepticks','keeplimits');
+    xlabel('Date');
+    ylabel('Peak Period (s)');
+    title(['M =' num2str(M(mm))]);
+end
+
+print(gcf,'-depsc2','ex24.eps');
